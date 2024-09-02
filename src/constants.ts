3e6ce05a7b1b95fcbaf966b6ee136bb62e506119
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-09-02 21:48:59 +0600

    refactor: Add BASE_URL constant for app's base URL
    To be used in 8424fd8

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,6 @@
 export const TIME_TIL_CHOICE_REVEAL = 5000;
 /** Time that the users have to answer a question in ms */
 export const QUESTION_ANSWER_TIME = 20000;
+/** Base URL for the app */
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
